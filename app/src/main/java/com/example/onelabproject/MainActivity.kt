93084bc843fcc18743d74cbf0243fcab3775ac27
Zhanarys94 <janaris1994@gmail.com>
2023-12-11 01:44:01 +0600

    App with 1 activity and 2 fragments

--- app/src/main/java/com/example/onelabproject/MainActivity.kt
+++ app/src/main/java/com/example/onelabproject/MainActivity.kt
@@ -2,15 +2,43 @@ package com.example.onelabproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import androidx.activity.viewModels
 import com.example.onelabproject.databinding.ActivityMainBinding
+import com.example.onelabproject.fragments.CityChooserFragment
+import com.example.onelabproject.fragments.TimeFragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val cityAndTimeViewModel: CityAndTimeViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .setReorderingAllowed(true)
+                .add(binding.mainActivityFragmentContainer.id, CityChooserFragment(), "CityChooserFragment")
+                .commit()
+        }
+        binding.switchCityToTime.setOnClickListener {
+            cityAndTimeViewModel.switchCityToTimeSwitcher()
+        }
+        cityAndTimeViewModel.isCityToTimeSwitcherActive.observe(this) { isSwitcherActive ->
+            when (isSwitcherActive) {
+                true -> {
+                    supportFragmentManager.beginTransaction()
+                        .setReorderingAllowed(true)
+                        .replace(binding.mainActivityFragmentContainer.id, TimeFragment(), "TimeFragment")
+                        .addToBackStack("TimeFragment")
+                        .commit()
+                }
+                false -> {
+                    supportFragmentManager.popBackStack("TimeFragment", 1)
+                }
+            }
+        }
     }
 }
