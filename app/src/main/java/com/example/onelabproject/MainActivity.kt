912ba9ec41fa1af8d5fce7c52f127b1f62ee4271
Zhanarys94 <janaris1994@gmail.com>
2023-12-10 17:45:10 +0600

    App with 2 activities

--- app/src/main/java/com/example/onelabproject/MainActivity.kt
+++ app/src/main/java/com/example/onelabproject/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.example.onelabproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import com.example.onelabproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +12,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.buttonMain.setOnClickListener {
+            val intent = Intent(this, ShowDigitActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
